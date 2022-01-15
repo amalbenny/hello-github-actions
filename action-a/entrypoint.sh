c7deda10953e389287fa867d94370ba09e4258ec
Amal Benny <alyod@pm.me>
2022-01-15 15:59:08 +0530

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME"
+sh -c "echo Hello world, my name is $INPUT_MY_NAME hope you're well. It's my first action file through github. Did you noticed that. It's cool right?"
