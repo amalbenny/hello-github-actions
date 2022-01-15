19dfa1b6f7a07e167354f076d42a2b7db7bc7078
Amal Benny <alyod@pm.me>
2022-01-15 16:12:04 +0530

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -
 
-sh -c "echo Hello world, my name is $INPUT_MY_NAME"
-sh -c "echo action: Am I working?"
+sh -c "echo me : Hello world, my name is $INPUT_MY_NAME"
+sh -c "echo action : Am I working?"
+sh -c "echo me : Yeah, absolutely ✓"
